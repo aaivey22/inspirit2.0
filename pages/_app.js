1a4d600217e072a647d198c58da03cd3a4791bea
aaivey22 <angelawings22@gmail.com>
2020-07-23 14:56:42 -0400

    fetching static data from productsJSON and moved over some code from test file

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,23 @@ import Layout from '../components/_App/Layout';
 // pages/_app provides access to every page component.
 // This allows us pass down data and is useful for authenticating users and passing their data from page to page.
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    return { pageProps: pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps  } = this.props;
     return (
-      <Layout>
-        <Component />
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
       </Layout>
     )
   }
 }
 
+// ... spread obj operator spreads in all obj props
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,32 @@ import React from 'react';
 import Axios from 'axios'; //http library
 
 
-function Home() {
-  // performs a sideEffect hook by making a request to an external api
-    React.useEffect(() => {
-    getProducts();
-  }, []);
+function Home({ products }) {
+  console.log(products);
+  // Performs a sideEffect hook by making a request to an external api
+  //   React.useEffect(() => {
+  //   getProducts();
+  // }, []);
 
-  // we receive the data and put it in a variable, this is possible with async funcs
-  // async returns a promise and resolves it with await
-  async function getProducts() {
-    const url = 'http://localhost:3000/api/products';
-    const response = await Axios.get(url);
-    console.log(response.data);
-  }
+  // // Receive the data and put it in a variable, this is possible with async funcs
+  // // Async returns a promise and resolves it with await
+  // async function getProducts() {
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await Axios.get(url);
+  //   console.log(response.data);
+  // }
 
   return <>home</>;
 }
-
+  Home.getInitialProps = async () => {
+    // This method receives data and add it to the Home component props
+    // Fetch data on server and return response data as an obj
+    // This obj is merged with existing props (will not overwrite)
+  
+    const url = 'http://localhost:3000/api/products';
+    // const url = `${baseUrl}/api/products`;
+    const response = await Axios.get(url);
+    return { products: response.data }
+  };
+  
 export default Home;
