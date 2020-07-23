5ae518e7fedfa0f29c12e8ed80c43c4fb0459a59
aaivey22 <angelawings22@gmail.com>
2020-07-23 14:36:57 -0400

    copied over code from test files and placed in indexJS and productsJS. ProductsJSON data is displaying in console.

---
+++ pages/api/products.js
@@ -0,0 +1,14 @@
+import products from '../../static/products.json'
+// import Product from '../../models/Product'
+// import connectDb from '../../utils/connectDb'
+
+// connectDb()
+
+export default (req, res) => {
+//    const products = await Product.find()
+   res.status(200).json(products)
+    console.log(req.method)
+};
+
+// the res obj returns meta info (status codes)
+// data will be returned to the client if successful via the json method
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
+import React from 'react';
+import Axios from 'axios'; //http library
+
+
 function Home() {
+  // performs a sideEffect hook by making a request to an external api
+    React.useEffect(() => {
+    getProducts();
+  }, []);
+
+  // we receive the data and put it in a variable, this is possible with async funcs
+  // async returns a promise and resolves it with await
+  async function getProducts() {
+    const url = 'http://localhost:3000/api/products';
+    const response = await Axios.get(url);
+    console.log(response.data);
+  }
+
   return <>home</>;
 }
 
