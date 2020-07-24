2551dbb782286e3e2620c4af82f48b6ad7b76838
aaivey22 <angelawings22@gmail.com>
2020-07-24 10:14:02 -0400

    changes to heroku url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl = 
     process.env.NODE_ENV === "production" 
-    ? 'https://git.heroku.com/evening-citadel-47188.git'
+    ? 'https://evening-citadel-47188.herokuapp.com'
     : 'http://localhost:3000';
 
 export default baseUrl;
