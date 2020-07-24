f8287b494e29cda620d79987143b4202fbb193b8
aaivey22 <angelawings22@gmail.com>
2020-07-24 17:38:01 -0400

    fetching user data to display on accountPage in accountJS and accountheaderJS

--- pages/account.js
+++ pages/account.js
@@ -1,5 +1,19 @@
-function Account() {
-  return <>account</>;
+import AccountHeader from '../components/Account/AccountHeader';
+import AccountOrders from '../components/Account/AccountOrders';
+import { parseCookies } from 'nookies';
+
+// destructure the user of the account that is being accessed and spread the user props into the header and orders components
+function Account({ user }) {
+  return (
+    <>
+      <AccountHeader {...user} />
+      <AccountOrders {...user} />
+    </>
+  )
+}
+
+Account.getInitialProps = async ctx => {
+  
 }
 
 export default Account;
