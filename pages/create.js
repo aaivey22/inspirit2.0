73594563c71ef3eabf7173e81e202a374bfd15e8
aaivey22 <angelawings22@gmail.com>
2020-07-30 16:55:57 -0400

    correcting syntax error to enable inventory posting

--- pages/create.js
+++ pages/create.js
@@ -55,7 +55,7 @@ function CreateProduct() {
       const url = `${baseUrl}/api/product`
       const { name, price, description } = product;
       const payload = { name, price, description, image };
-      const response = await axios.post(url, payload);
+      await axios.post(url, payload);
       // console.log({response})
       setProduct(INITIAL_PRODUCT)
       setSuccess(true)
